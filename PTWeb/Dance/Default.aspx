07148043577c347f7835cac9ca78434c9e158417
joker41649539 <70456515+joker41649539@users.noreply.github.com>
2021-03-13 13:04:51 +0800

    1
    1

---
+++ PTWeb/Dance/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Dance/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Dance_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="row">
+        <div class="col-sm-12">
+            <h3 class="header smaller lighter red">星期一</h3>
+
+            <div class="well">
+                <h4 class="green smaller lighter">下午 14:00 - 15:00</h4>
+                哈他瑜伽 
+                <h4 class="green smaller lighter">戈老师</h4>
+                
+            </div>
+
+            <div class="well well-lg">
+                <h4 class="blue">Large Well</h4>
+                Control padding and rounded corners with two optional modifier classes.
+            </div>
+            <div class="well well-sm">This is a small well </div>
+        </div>
+    </div>
+    <!-- /span -->
+</asp:Content>
+
---
+++ PTWeb/Dance/MasterPage.master.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Fil_MasterPage : System.Web.UI.MasterPage
+{
+    /// <summary>
+    /// 数据库连接字符串
+    /// </summary>
+    public static string DefaultConStr = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+
+    OpMode OP_Mode = new OpMode(DefaultConStr, 0);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadUserInfo();
+            // MessageBox("消息提示","测试信息");
+        }
+    }
+
+    private bool LoadUserInfo()
+    {
+        bool rValue = false;
+
+        //string strSQL = "Select * from Fil_Users Where ID=2";
+        //if (OP_Mode.SQLRUN(strSQL))
+        //{
+        //    if (OP_Mode.Dtv.Count > 0)
+        //    {
+        //        // 临时登录
+        //        /// 如果数据库有ID，则直接登录。
+        //        Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_USERID] = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+        //        Response.Cookies["WeChat_Yanwo"]["USERID"] = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+        //        Response.Cookies["WeChat_Yanwo"]["COPENID"] = "";
+        //        Response.Cookies["WeChat_Yanwo"]["CNAME"] = OP_Mode.Dtv[0]["Nick"].ToString().Trim();
+        //        Response.Cookies["WeChat_Yanwo"]["LTIME"] = OP_Mode.Dtv[0]["LTIME"].ToString().Trim();
+        //        Response.Cookies["WeChat_Yanwo"]["HEADURL"] = OP_Mode.Dtv[0]["HeadImage"].ToString().Trim();
+
+        //        Response.Cookies["WeChat_Yanwo"]["LOGIN"] = "true";
+
+        //        Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_CNAME] = OP_Mode.Dtv[0]["Nick"].ToString().Trim();
+        //        Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_CTX] = OP_Mode.Dtv[0]["HeadImage"].ToString().Trim();
+
+        //        ///设置COOKIE最长时间
+        //        Response.Cookies["WeChat_Yanwo"].Expires = DateTime.MaxValue;
+
+
+        //        /// 给用户ID赋值
+        //        HiddenField_UserID.Value = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+        //        Label_Nick.Text = OP_Mode.Dtv[0]["Nick"].ToString().Trim();
+        //        if (OP_Mode.Dtv[0]["HeadImage"].ToString().Trim().Length > 0)
+        //        {
+        //            Image_Header.ImageUrl = OP_Mode.Dtv[0]["HeadImage"].ToString().Trim();
+        //        }
+        //        /// 更新登录时间
+        //        OP_Mode.SQLRUN("Update Fil_Users set Ltime=getdate() where ID=2");
+        //    }
+        //}
+
+
+        return rValue;
+    }
+    /// <summary>
+    /// 微信登录
+    /// </summary>
+    private void WeChatLoad()
+    {
+        string accessToken = string.Empty;
+        string DeBugMsg = string.Empty;
+
+        string AppId = WebConfigurationManager.AppSettings["CorpId"];//与微信公众账号后台的AppId设置保持一致，区分大小写。
+        string AppSecret = WebConfigurationManager.AppSettings["WeixinAppSecret"];
+
+        var code = string.Empty;
+        var opentid = string.Empty;
+        try
+        {
+            code = Request.QueryString["code"];
+            DeBugMsg += "code:" + code;
+        }
+        catch
+        {
+
+        }
+
+        if (string.IsNullOrEmpty(code))
+        {
+
+        }
+        else
+        {
+            string strWeixin_OpenID = string.Empty;
+
+            string STRUSERID = string.Empty;
+
+            if (strWeixin_OpenID == string.Empty || STRUSERID == string.Empty)
+            {
+                DeBugMsg += "<br> 没有所需的OPENID！";
+
+                // this.Label1.Text = "没有所需的OPENID";
+                var client = new System.Net.WebClient();
+                client.Encoding = System.Text.Encoding.UTF8;
+
+                var url = string.Format("https://api.weixin.qq.com/sns/oauth2/access_token?appid={0}&secret={1}&code={2}&grant_type=authorization_code", AppId, AppSecret, code);
+                var data = client.DownloadString(url);
+                var serializer = new JavaScriptSerializer();
+                var obj = serializer.Deserialize<Dictionary<string, string>>(data);
+
+                if (!obj.TryGetValue("access_token", out accessToken))
+                {
+                    DeBugMsg += "<br> Token获取错误！";
+                }
+                else
+                {
+                    opentid = obj["openid"];
+                    //MessageBox("", "opentid：" + opentid);
+                }
+
+                // string tempToken = GetaccessWebToken(code);
+
+                url = string.Format("https://api.weixin.qq.com/sns/userinfo?access_token={0}&openid={1}&lang=zh_CN", accessToken, opentid);
+                data = client.DownloadString(url);
+                var userInfo = serializer.Deserialize<Dictionary<string, object>>(data);
+                DeBugMsg += "userInfo：" + data.ToString();
+                int vsex = 2;
+
+                var vcity = "中国";
+
+                if (userInfo["sex"].ToString() == "1")
+                {
+                    vsex = 0;
+                }
+
+                vcity = userInfo["country"].ToString() + userInfo["province"].ToString() + userInfo["city"].ToString();
+                DeBugMsg += "城市：" + vcity;
+                if (opentid.Length == 0)
+                {
+                    opentid = "test111";
+                }
+                string UserName = "testName";
+                string HeadUserUrl = "";
+
+                UserName = userInfo["nickname"].ToString();
+                DeBugMsg += "昵称：" + UserName;
+                HeadUserUrl = userInfo["headimgurl"].ToString();
+
+                DeBugMsg += "头像：" + HeadUserUrl;
+
+
+                string strSQL;
+                strSQL = " Select * from S_USERINFO where COPENID='" + opentid.ToString() + "'";
+
+                if (OP_Mode.SQLRUN(strSQL))
+                {
+                    if (OP_Mode.Dtv.Count > 0)
+                    {
+                        if (Convert.ToInt32(OP_Mode.Dtv[0]["flag"]) != 0)
+                        {
+                            MessageBox("", "您被禁止登陆！<br>请联系管理员。", "/Login.aspx");
+                            return;
+                        }
+                        /// 如果数据库有ID，则直接登录。
+                        Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_USERID] = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+                        Response.Cookies["WeChat_Yanwo"]["USERID"] = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+                        Response.Cookies["WeChat_Yanwo"]["COPENID"] = opentid.ToString();
+                        Response.Cookies["WeChat_Yanwo"]["CNAME"] = HttpUtility.UrlEncode(UserName);
+                        Response.Cookies["WeChat_Yanwo"]["LTIME"] = OP_Mode.Dtv[0]["LTIME"].ToString().Trim();
+                        Response.Cookies["WeChat_Yanwo"]["HEADURL"] = HeadUserUrl;
+
+                        Response.Cookies["WeChat_Yanwo"]["LOGIN"] = "true";
+
+                        Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_CNAME] = UserName;
+                        Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_CTX] = HeadUserUrl;
+
+                        ///设置COOKIE最长时间
+                        Response.Cookies["WeChat_Yanwo"].Expires = DateTime.MaxValue;
+
+                        /// 更新登录时间
+                        OP_Mode.SQLRUN("Update S_USERINFO set Ltime=getdate(),HEADURL='" + HeadUserUrl + "',XB=" + vsex + " where COPENID='" + opentid.ToString() + "'");
+
+                        return;
+                    }
+                    else
+                    {
+                        try
+                        {
+
+                            strSQL = " INSERT INTO S_USERINFO (LOGINNAME,PASSWORD,COPENID,CNAME,HEADURL,XB) VALUES ('" + opentid + "','" + opentid + "','" + opentid + "','" + UserName + "','" + HeadUserUrl + "'," + vsex + ")";
+
+                            strSQL += " Select * from S_USERINFO where COPENID='" + opentid + "'";
+
+                            DeBugMsg += "+" + strSQL + "+";
+
+                            if (OP_Mode.SQLRUN(strSQL))
+                            {
+                                if (OP_Mode.Dtv.Count > 0)
+                                {
+                                    Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_USERID] = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+                                    Response.Cookies["WeChat_Yanwo"]["USERID"] = OP_Mode.Dtv[0]["ID"].ToString().Trim();
+                                    Response.Cookies["WeChat_Yanwo"]["COPENID"] = OP_Mode.Dtv[0]["COPENID"].ToString().Trim();
+                                    Response.Cookies["WeChat_Yanwo"]["CNAME"] = HttpUtility.UrlEncode(OP_Mode.Dtv[0]["CNAME"].ToString()); //HttpUtility.UrlDecode(Request.Cookies["SK_WZGY"]["CNAME"].ToString().Trim(), Encoding.GetEncoding("UTF-8"))
+                                    Response.Cookies["WeChat_Yanwo"]["LTIME"] = OP_Mode.Dtv[0]["LTIME"].ToString().Trim();
+                                    Response.Cookies["WeChat_Yanwo"]["HEADURL"] = OP_Mode.Dtv[0]["HEADURL"].ToString().Trim();
+
+                                    Response.Cookies["WeChat_Yanwo"][Constant.COOKIENAMEUSER_CNAME] = OP_Mode.Dtv[0]["CNAME"].ToString().Trim();
+                                    Response.Cookies[Constant.COOKIENAMEUSER][Constant.COOKIENAMEUSER_CTX] = OP_Mode.Dtv[0]["HEADURL"].ToString().Trim();
+
+                                    Response.Cookies["WeChat_Yanwo"]["LOGIN"] = "true";
+                                    Response.Cookies[Constant.COOKIENAMEOPENDOOR][Constant.COOKIENAMEOPENDOOR_LGOIN] = "true";
+                                    ///设置COOKIE最长时间  不设置时间，窗口关闭则丢失
+                                    Response.Cookies["WeChat_Yanwo"].Expires = DateTime.MaxValue;
+
+                                    string MSG = string.Empty;// string.Format("<img class=\"img-rounded\" src=\"{1}\" width=\"60PX\" />欢迎 {0} 注册成功。<br/>祝您生活愉快。", OP_Mode.Dtv[0]["CNAME"].ToString(), OP_Mode.Dtv[0]["HEADURL"].ToString());
+
+                                    MSG = "<img class=\"img-rounded\" src=\"" + OP_Mode.Dtv[0]["HEADURL"].ToString() + "\" width=\"60PX\" />欢迎 " + OP_Mode.Dtv[0]["CNAME"].ToString() + " 注册成功。<br/>祝您生活愉快。";
+
+                                    MessageBox("", MSG);
+
+                                    return;
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            DeBugMsg += "<br>" + ex.ToString();
+                            MessageBox("", "4：" + DeBugMsg);
+                        }
+                    }
+                }
+                else
+                {
+                    DeBugMsg += OP_Mode.strErrMsg;
+                    MessageBox("", "5：" + DeBugMsg);
+                }
+
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sKey"></param>
+    /// <param name="sMessage"></param>
+    public void MessageBox(string sKey, string sMessage)
+    {
+        if (sKey == "")
+        {
+            sKey = "提示信息";
+        }
+        string sTemp = sMessage;
+        sTemp = sTemp.Replace("\r", @"\\r");
+        sTemp = sTemp.Replace("\n", @"\\n");
+        sTemp = sTemp.Replace("'", @"\'");    // javascript 中使用"\'"显示'字符。
+        this.Page.ClientScript.RegisterStartupScript(typeof(string), "", "<script src=\"/assets/js/jquery-2.0.3.min.js\"></script> <script language=JavaScript>dialog = jqueryAlert({ 'title': '" + sKey + "', 'content': '" + sTemp + "', 'modal': true, 'buttons': { '确定': function () {dialog.destroy();dialog.close();} } })</script>");
+    }
+
+    /// <summary>
+    /// 模态框消息提示，并跳转页面
+    /// </summary>
+    /// <param name="sKey">提示窗口标题</param>
+    /// <param name="sMessage">消息内容</param>
+    /// <param name="sURL">提示后跳转页面</param>
+    public void MessageBox(string sKey, string sMessage, string sURL)
+    {
+        if (sKey == "")
+        {
+            sKey = "提示信息";
+        }
+        string sTemp = sMessage;
+        sTemp = sTemp.Replace("\r", @"\\r");
+        sTemp = sTemp.Replace("\n", @"\\n");
+        sTemp = sTemp.Replace("'", @"\'");    // javascript 中使用"\'"显示'字符。
+        this.Page.ClientScript.RegisterStartupScript(typeof(string), "", "<script src=\"/assets/js/jquery-2.0.3.min.js\"></script> <script language=JavaScript>dialog = jqueryAlert({ 'title': '" + sKey + "', 'content': '" + sTemp + "', 'modal': true, 'buttons': { '确定': function () { location.href=\"" + sURL + "\"; } } })</script>");
+    }
+
+}
